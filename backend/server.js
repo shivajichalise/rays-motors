785f1c7dda405e72eb54ebc2c0af3cf1cede80cc
Shivaji Chalise <respond2shivaji@gmail.com>
2021-12-31 18:04:32 +0545

    fixing url

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,9 @@ if (process.env.NODE_ENV === 'production') {
   app.get(
     '*',
     (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      )
 
     // below for cpanel deployment
     // res.sendFile(path.resolve(__dirname, '../public_html', 'index.html'))
